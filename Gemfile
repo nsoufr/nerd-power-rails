cd6f1403f5fb3366e576fdd023f451feab53014f
Nando Sousa <nandosousafr@gmail.com>
2014-01-17 16:56:29 -0300

    commit inicial

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+gem 'pg'
+gem 'rails', '4.0.2'
+gem 'sass-rails', '~> 4.0.0'
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nerdpower::Application.config.secret_key_base = 'ec80e28d826130f7e4f17ba9f3e6985b4d9caf7340d2a8b8b5b16b40f1a372cb23c0124e3941982716fad6d926e8a7b5f5479097fa51818d07e796c8a5bc5454'
