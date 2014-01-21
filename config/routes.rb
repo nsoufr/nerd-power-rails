00f36d7162f86565d91c1d830f171c4f7d36f012
Nando Sousa <nandosousafr@gmail.com>
2014-01-20 21:34:44 -0300

    added nerd profile and nerd feed page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Nerdpower::Application.routes.draw do
   root 'home#index'
+
+  scope :user do
+    get '/' => 'user#index',          as: :user_feed
+    get '/profile' => 'user#profile', as: :user_profile
+  end
 end
