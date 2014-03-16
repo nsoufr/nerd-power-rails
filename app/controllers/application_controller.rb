5c80d4f173caae48761e4f59ad9469582233e6eb
Nando Sousa <nandosousafr@gmail.com>
2014-03-16 17:27:09 -0300

    added missing fields in signup page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up).concat([:nickname, :name])
+  end
+
+  def after_sign_in_path_for(resource)
+   user_feed_path
+  end
 end
