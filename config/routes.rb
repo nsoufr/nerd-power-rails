6a3963bfe2d79c05cf747f6f055b057e90334439
Nando Sousa <nandosousafr@gmail.com>
2014-03-16 14:01:06 -0300

    requiring user auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Nerdpower::Application.routes.draw do
   devise_for :users
   root 'home#index'
 
-  scope :user do
-    get '/' => 'user#index',          as: :user_feed
-    get '/profile' => 'user#profile', as: :user_profile
+  authenticate :user do
+    scope :user do
+      get '/' => 'user#index',          as: :user_feed
+      get '/profile' => 'user#profile', as: :user_profile
+    end
   end
 end
