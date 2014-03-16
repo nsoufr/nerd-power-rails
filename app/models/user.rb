aac53d8f6185013c29759d1775f00b49c34fe132
Nando Sousa <nandosousafr@gmail.com>
2014-03-16 14:00:09 -0300

    added User#avatar using gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,9 @@ class User < ActiveRecord::Base
   has_many :pendent_invitations, class_name: 'Invitation', foreign_key: :sender_id
   has_many :inbox_invitations, class_name: 'Invitation', foreign_key: :receiver_id
   has_many :posts
+
+  def avatar(size = 70)
+    hash = Digest::MD5.hexdigest(email)
+    "http://www.gravatar.com/avatar/#{hash}?s=#{size}&d=identicon&r=G"
+  end
 end
