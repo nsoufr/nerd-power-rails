b9abbb6cf7c81b23bfa81c85cc45fbb80060be88
bruno_tripoloni <bruno.tripoloni@gmail.com>
2014-03-25 13:54:00 -0300

    suporte inicial ao angularJS

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-  	if user_signed_in?
-  		redirect_to user_feed_path, alert: "Por favor, Faça login ou Cadastre-se"
-  	end
+  	#if user_signed_in?
+  	#	redirect_to user_feed_path, alert: "Por favor, Faça login ou Cadastre-se"
+  	#end
   end
 end
