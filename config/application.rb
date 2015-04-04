4e0469aeaaa0dd97dea6c10a1ec8a64eafaab6df
Nando Sousa <nandosousafr@gmail.com>
2015-04-04 19:09:25 -0300

    adds comment and like insights to post card

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,14 @@ module Grokspot
     config.assets.initialize_on_precompile = false
 
     # Supports bower
-    config.assets.append_path 'components'
+    root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+      config.sass.load_paths << bower_path
+      config.assets.paths << bower_path
+    end
+    # Precompile Bootstrap fonts
+    config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+    # Minimum Sass number precision required by bootstrap-sass
+    ::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max    config.assets.append_path 'components/bootstrap-sass/assets'
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
